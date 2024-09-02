9670965dd19a8634e1e29fcbe7831b7db9f7fb29
Kayce Basques <kayce@google.com>
2024-09-01 20:38:34 -0700

    add i2c screen stuff

--- main.c
+++ main.c
@@ -1,33 +1,286 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "font.h"
 
-#ifndef LED_DELAY_MS
-#define LED_DELAY_MS 250
-#endif
+#define SSD1306_HEIGHT              32
+#define SSD1306_WIDTH               128
+#define SSD1306_I2C_ADDR            _u(0x3C)
+#define SSD1306_I2C_CLK             400
+#define SSD1306_SET_MEM_MODE        _u(0x20)
+#define SSD1306_SET_COL_ADDR        _u(0x21)
+#define SSD1306_SET_PAGE_ADDR       _u(0x22)
+#define SSD1306_SET_HORIZ_SCROLL    _u(0x26)
+#define SSD1306_SET_SCROLL          _u(0x2E)
+#define SSD1306_SET_DISP_START_LINE _u(0x40)
+#define SSD1306_SET_CONTRAST        _u(0x81)
+#define SSD1306_SET_CHARGE_PUMP     _u(0x8D)
+#define SSD1306_SET_SEG_REMAP       _u(0xA0)
+#define SSD1306_SET_ENTIRE_ON       _u(0xA4)
+#define SSD1306_SET_ALL_ON          _u(0xA5)
+#define SSD1306_SET_NORM_DISP       _u(0xA6)
+#define SSD1306_SET_INV_DISP        _u(0xA7)
+#define SSD1306_SET_MUX_RATIO       _u(0xA8)
+#define SSD1306_SET_DISP            _u(0xAE)
+#define SSD1306_SET_COM_OUT_DIR     _u(0xC0)
+#define SSD1306_SET_COM_OUT_DIR_FLIP _u(0xC0)
+#define SSD1306_SET_DISP_OFFSET     _u(0xD3)
+#define SSD1306_SET_DISP_CLK_DIV    _u(0xD5)
+#define SSD1306_SET_PRECHARGE       _u(0xD9)
+#define SSD1306_SET_COM_PIN_CFG     _u(0xDA)
+#define SSD1306_SET_VCOM_DESEL      _u(0xDB)
+#define SSD1306_PAGE_HEIGHT         _u(8)
+#define SSD1306_NUM_PAGES           (SSD1306_HEIGHT / SSD1306_PAGE_HEIGHT)
+#define SSD1306_BUF_LEN             (SSD1306_NUM_PAGES * SSD1306_WIDTH)
+#define SSD1306_WRITE_MODE         _u(0xFE)
+#define SSD1306_READ_MODE          _u(0xFF)
 
-// Initialize the GPIO for the LED
-void pico_led_init(void) {
-#ifdef PICO_DEFAULT_LED_PIN
-    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
-    // so we can use normal GPIO functionality to turn the led on and off
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-#endif
+
+struct render_area {
+    uint8_t start_col;
+    uint8_t end_col;
+    uint8_t start_page;
+    uint8_t end_page;
+
+    int buflen;
+};
+
+void calc_render_area_buflen(struct render_area *area) {
+    // calculate how long the flattened buffer will be for a render area
+    area->buflen = (area->end_col - area->start_col + 1) * (area->end_page - area->start_page + 1);
 }
 
-// Turn the LED on or off
-void pico_set_led(bool led_on) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    // Just set the GPIO on or off
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#endif
+void SSD1306_send_cmd(uint8_t cmd) {
+    // I2C write process expects a control byte followed by data
+    // this "data" can be a command or data to follow up a command
+    // Co = 1, D/C = 0 => the driver expects a command
+    uint8_t buf[2] = {0x80, cmd};
+    i2c_write_blocking(i2c_default, SSD1306_I2C_ADDR, buf, 2, false);
 }
+
+void SSD1306_send_cmd_list(uint8_t *buf, int num) {
+    for (int i=0;i<num;i++)
+        SSD1306_send_cmd(buf[i]);
+}
+
+void SSD1306_send_buf(uint8_t buf[], int buflen) {
+    // in horizontal addressing mode, the column address pointer auto-increments
+    // and then wraps around to the next page, so we can send the entire frame
+    // buffer in one gooooooo!
+
+    // copy our frame buffer into a new buffer because we need to add the control byte
+    // to the beginning
+
+    uint8_t *temp_buf = malloc(buflen + 1);
+
+    temp_buf[0] = 0x40;
+    memcpy(temp_buf+1, buf, buflen);
+
+    i2c_write_blocking(i2c_default, SSD1306_I2C_ADDR, temp_buf, buflen + 1, false);
+
+    free(temp_buf);
+}
+
+void SSD1306_init() {
+    // Some of these commands are not strictly necessary as the reset
+    // process defaults to some of these but they are shown here
+    // to demonstrate what the initialization sequence looks like
+    // Some configuration values are recommended by the board manufacturer
+
+    uint8_t cmds[] = {
+        SSD1306_SET_DISP,               // set display off
+        /* memory mapping */
+        SSD1306_SET_MEM_MODE,           // set memory address mode 0 = horizontal, 1 = vertical, 2 = page
+        0x00,                           // horizontal addressing mode
+        /* resolution and layout */
+        SSD1306_SET_DISP_START_LINE,    // set display start line to 0
+        SSD1306_SET_SEG_REMAP | 0x01,   // set segment re-map, column address 127 is mapped to SEG0
+        SSD1306_SET_MUX_RATIO,          // set multiplex ratio
+        SSD1306_HEIGHT - 1,             // Display height - 1
+        SSD1306_SET_COM_OUT_DIR | 0x08, // set COM (common) output scan direction. Scan from bottom up, COM[N-1] to COM0
+        SSD1306_SET_DISP_OFFSET,        // set display offset
+        0x00,                           // no offset
+        SSD1306_SET_COM_PIN_CFG,        // set COM (common) pins hardware configuration. Board specific magic number.
+                                        // 0x02 Works for 128x32, 0x12 Possibly works for 128x64. Other options 0x22, 0x32
+        0x02,
+        /* timing and driving scheme */
+        SSD1306_SET_DISP_CLK_DIV,       // set display clock divide ratio
+        0x80,                           // div ratio of 1, standard freq
+        SSD1306_SET_PRECHARGE,          // set pre-charge period
+        0xF1,                           // Vcc internally generated on our board
+        SSD1306_SET_VCOM_DESEL,         // set VCOMH deselect level
+        0x30,                           // 0.83xVcc
+        /* display */
+        SSD1306_SET_CONTRAST,           // set contrast control
+        0xFF,
+        SSD1306_SET_ENTIRE_ON,          // set entire display on to follow RAM content
+        SSD1306_SET_NORM_DISP,           // set normal (not inverted) display
+        SSD1306_SET_CHARGE_PUMP,        // set charge pump
+        0x14,                           // Vcc internally generated on our board
+        SSD1306_SET_SCROLL | 0x00,      // deactivate horizontal scrolling if set. This is necessary as memory writes will corrupt if scrolling was enabled
+        SSD1306_SET_DISP | 0x01, // turn display on
+    };
+
+    SSD1306_send_cmd_list(cmds, count_of(cmds));
+}
+
+void SSD1306_scroll(bool on) {
+    // configure horizontal scrolling
+    uint8_t cmds[] = {
+        SSD1306_SET_HORIZ_SCROLL | 0x00,
+        0x00, // dummy byte
+        0x00, // start page 0
+        0x00, // time interval
+        0x03, // end page 3 SSD1306_NUM_PAGES ??
+        0x00, // dummy byte
+        0xFF, // dummy byte
+        SSD1306_SET_SCROLL | (on ? 0x01 : 0) // Start/stop scrolling
+    };
+
+    SSD1306_send_cmd_list(cmds, count_of(cmds));
+}
+
+void render(uint8_t *buf, struct render_area *area) {
+    // update a portion of the display with a render area
+    uint8_t cmds[] = {
+        SSD1306_SET_COL_ADDR,
+        area->start_col,
+        area->end_col,
+        SSD1306_SET_PAGE_ADDR,
+        area->start_page,
+        area->end_page
+    };
+
+    SSD1306_send_cmd_list(cmds, count_of(cmds));
+    SSD1306_send_buf(buf, area->buflen);
+}
+
+static inline int GetFontIndex(uint8_t ch) {
+    if (ch >= 'A' && ch <='Z') {
+        return  ch - 'A' + 1;
+    }
+    else if (ch >= '0' && ch <='9') {
+        return  ch - '0' + 27;
+    }
+    else return  0; // Not got that char so space.
+}
+
+static void WriteChar(uint8_t *buf, int16_t x, int16_t y, uint8_t ch) {
+    if (x > SSD1306_WIDTH - 8 || y > SSD1306_HEIGHT - 8)
+        return;
+
+    // For the moment, only write on Y row boundaries (every 8 vertical pixels)
+    y = y/8;
+
+    ch = toupper(ch);
+    int idx = GetFontIndex(ch);
+    int fb_idx = y * 128 + x;
+
+    for (int i=0;i<8;i++) {
+        buf[fb_idx++] = font[idx * 8 + i];
+    }
+}
+
+static void WriteString(uint8_t *buf, int16_t x, int16_t y, char *str) {
+    // Cull out any string off the screen
+    if (x > SSD1306_WIDTH - 8 || y > SSD1306_HEIGHT - 8)
+        return;
+
+    while (*str) {
+        WriteChar(buf, x, y, *str++);
+        x+=8;
+    }
+}
+
 
 int main() {
-    pico_led_init();
-    while (true) {
-        pico_set_led(true);
-        sleep_ms(LED_DELAY_MS);
-        pico_set_led(false);
-        sleep_ms(LED_DELAY_MS);
+    stdio_init_all();
+
+    i2c_init(i2c_default, SSD1306_I2C_CLK * 1000);
+    gpio_set_function(20, GPIO_FUNC_I2C);
+    gpio_set_function(21, GPIO_FUNC_I2C);
+    gpio_pull_up(20);
+    gpio_pull_up(21);
+
+    // run through the complete initialization process
+    SSD1306_init();
+
+    // Initialize render area for entire frame (SSD1306_WIDTH pixels by SSD1306_NUM_PAGES pages)
+    struct render_area frame_area = {
+        start_col: 0,
+        end_col : SSD1306_WIDTH - 1,
+        start_page : 0,
+        end_page : SSD1306_NUM_PAGES - 1
+        };
+
+    calc_render_area_buflen(&frame_area);
+
+    // zero the entire display
+    uint8_t buf[SSD1306_BUF_LEN];
+    memset(buf, 0, SSD1306_BUF_LEN);
+    render(buf, &frame_area);
+
+    // intro sequence: flash the screen 3 times
+    for (int i = 0; i < 3; i++) {
+        SSD1306_send_cmd(SSD1306_SET_ALL_ON);    // Set all pixels on
+        sleep_ms(500);
+        SSD1306_send_cmd(SSD1306_SET_ENTIRE_ON); // go back to following RAM for pixel state
+        sleep_ms(500);
     }
+
+restart:
+
+
+    char *text[] = {
+        "A long time ago",
+        "  on an OLED ",
+        "   display",
+        " far far away",
+        "Lived a small",
+        "red raspberry",
+        "by the name of",
+        "    PICO"
+    };
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++) {
+        WriteString(buf, 5, y, text[i]);
+        y += 8;
+        // Height limit reached. Show some lines.
+        if (y == SSD1306_HEIGHT) {
+            render(buf, &frame_area);
+            sleep_ms(3000);
+            memset(buf, 0, SSD1306_BUF_LEN);
+            y = 0;
+        }
+    }
+    // Check if there's any more text left to display.
+    if (y != 0) {
+        render(buf, &frame_area);
+        sleep_ms(3000);
+    }
+
+    SSD1306_send_cmd(SSD1306_SET_INV_DISP);
+    sleep_ms(1000);
+    SSD1306_send_cmd(SSD1306_SET_NORM_DISP);
+    sleep_ms(1000);
+    SSD1306_send_cmd(SSD1306_SET_INV_DISP);
+    sleep_ms(1000);
+    SSD1306_send_cmd(SSD1306_SET_NORM_DISP);
+
+    // Scroll the text off the screen
+    SSD1306_scroll(true);
+    sleep_ms(5000);
+    SSD1306_scroll(false);
+
+    // Clear the screen
+    memset(buf, 0, SSD1306_BUF_LEN);
+    render(buf, &frame_area);
+
+    goto restart;
+
+    return 0;
 }
