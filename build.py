2e0003e85629bffcc01305ece2944550c677bda7
Kayce Basques <kayce@google.com>
2024-09-01 21:53:25 -0700

    start building picotool from source

--- build.py
+++ build.py
@@ -1,12 +1,10 @@
 import os
 import subprocess
 
-os.environ['PICO_SDK_PATH'] = f'{os.getcwd()}/pico-sdk'
-
 # result = subprocess.run(['nproc'], stdout=subprocess.PIPE)
 # nproc = result.stdout.decode('utf-8')
 # print(nproc)
 
 os.chdir('build')
-subprocess.run(['make', '-j16'])  # TODO: Don't hardcode
+subprocess.run(['make', '-j16'])
 os.chdir('..')
--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 import os
 import subprocess
 
+pico_sdk_path = f'{os.getcwd()}/pico-sdk'
+
+subprocess.run([
+    'sudo', 'apt', 'install', 'cmake', 'gcc-arm-none-eabi',
+    'libnewlib-arm-none-eabi', 'build-essential', 'g++',
+    'libstdc++-arm-none-eabi-newlib', 'pkg-config',
+    'libusb-1.0-0-dev'
+])
+
 if not os.path.isdir('pico-sdk'):
     subprocess.run([
         'git', 'clone', 'git@github.com:kaycebasques/pico-sdk.git'
@@ -10,14 +19,33 @@ if not os.path.isdir('pico-sdk'):
         'git@github.com:raspberrypi/pico-sdk.git'
     ])
 
-subprocess.run([
-    'sudo', 'apt', 'install', 'cmake', 'gcc-arm-none-eabi',
-    'libnewlib-arm-none-eabi', 'build-essential', 'g++',
-    'libstdc++-arm-none-eabi-newlib'
-])
+if not os.path.isdir('picotool'):
+    # Don't use --recursive because we don't need mbedtls
+    subprocess.run([
+        'git', 'clone', 'git@github.com:kaycebasques/picotool.git'
+    ])
+    subprocess.run([
+        'git', '-C', 'picotool', 'remote', 'add', 'upstream',
+        'git@github.com:raspberrypi/picotool.git'
+    ])
+    os.chdir('picotool')
+    os.mkdir('build')
+    os.chdir('build')
+    subprocess.run([
+        'cmake', '..', f'-DPICO_SDK_PATH={pico_sdk_path}',
+        '-DCMAKE_INSTALL_PREFIX=../../bin', '-DPICOTOOL_FLAT_INSTALL=1'
+    ])
+    subprocess.run(['make', '-j16'])
+    subprocess.run(['make', 'install'])
+    os.chdir('../..')
 
 if not os.path.isdir('build'):
     os.mkdir('build')
     os.chdir('build')
-    subprocess.run(['cmake', '..'])
+    subprocess.run([
+        'cmake', '..', f'-DPICO_SDK_PATH={pico_sdk_path}',
+        '-DPICO_PLATFORM=rp2040',
+        '-DPICO_BOARD=pico', '-DPICO_COMPILER=pico_arm_cortex_m0plus_gcc',
+        '-DPICO_GCC_TRIPLE=arm-none-eabi', '-DCMAKE_INSTALL_PREFIX=../bin'
+    ])
     os.chdir('..')
